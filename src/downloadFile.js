9b2ce5ca67835e4135dc3e4e8ab44f38f69fd52a
Ken Corbett <ken@truepic.com>
2023-05-17 09:24:32 -0400

    Add retry logic to the file downloader

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -9,16 +9,26 @@ fs.mkdirSync(downloadDirectory);
 
 export default async function downloadFile(url, filename) {
   console.log("Downloading file: " + filename);
-  const response = await fetch(url, {
-    timeout: 1000 * 60 * 5, // 5 minutes
-    agent: new https.Agent({ keepAlive: true }),
-  });
-  const fileStream = fs.createWriteStream(
-    `${downloadDirectory}/${filename}.mp4`
-  );
-  await new Promise((resolve, reject) => {
-    response.body.pipe(fileStream);
-    response.body.on("error", reject);
-    fileStream.on("finish", resolve);
-  });
+  let retryCount = 0;
+  while (retryCount < 3) {
+    try {
+      const response = await fetch(url, {
+        timeout: 1000 * 60 * 5, // 5 minutes
+        agent: new https.Agent({ keepAlive: true }),
+      });
+      const fileStream = fs.createWriteStream(
+        `${downloadDirectory}/${filename}.mp4`
+      );
+      await new Promise((resolve, reject) => {
+        response.body.pipe(fileStream);
+        response.body.on("error", reject);
+        fileStream.on("finish", resolve);
+      });
+      return true;
+    } catch (error) {
+      console.error("Download failed: " + filename);
+      retryCount++;
+    }
+  }
+  console.error("Maximum retries reached. Download failed: " + filename);
 }
